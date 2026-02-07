6b4e59287fd6c1a53ee298aa6e8c81089cde2510
そめか <someka.vrc@gmail.com>
2026-02-07 20:32:00 +0900

    Set site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.somekasu.com/',
 	integrations: [
 		starlight({
 			title: 'そめか屋',
